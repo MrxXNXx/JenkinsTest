7f3759aa5d78249e93ec949cc79672ae2bdd1fad
KenlerMrz <1284974404@qq.com>
2021-04-26 01:35:39 +0800

    Update JenkinsApplication.java
    aaaaaa?

--- src/main/java/com/jen/jenkins/JenkinsApplication.java
+++ src/main/java/com/jen/jenkins/JenkinsApplication.java
@@ -3,7 +3,7 @@ package com.jen.jenkins;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
-ddddddddd
+**dddddddddddd
  */
 @SpringBootApplication
 public class JenkinsApplication {
